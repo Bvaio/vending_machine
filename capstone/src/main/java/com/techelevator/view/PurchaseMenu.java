a2df7c8e6cf2447c6f9e61e5f1d2546262cc8432
Thomas Lee <thmasleemail@gmail.com>
2022-04-02 09:17:28 -0400

    removed merge conflict holdovers

--- capstone/src/main/java/com/techelevator/view/PurchaseMenu.java
+++ capstone/src/main/java/com/techelevator/view/PurchaseMenu.java
@@ -49,10 +49,9 @@ public class PurchaseMenu extends Menu {
 
                 String scan = getScan().nextLine().toUpperCase();
                 final BigDecimal previousBalance = readBalance().getBalance();
-<<<<<<< HEAD
+
                 getLogger().writeItemPurchase(scan,readBalance().getBalance());
                 readBalance().payForItem( getPulledInventory().get( scan ) );
-=======
 
                 try {
                     readBalance().payForItem( getPulledInventory().get( scan ) );
@@ -60,7 +59,6 @@ public class PurchaseMenu extends Menu {
                     System.out.println( "Item not in list");
                     showMenu();
                 }
->>>>>>> a04995de1dac45e468943239591c21737654308a
 
                 if ( !previousBalance.equals( readBalance().getBalance() ) ) {
                     userEats(getPulledInventory().get( scan ) );
