f3bc20a704427f756f7be827af3bd16ae0b55396
Andreas Ramirez <andreas.j.ramirez@gmail.com>
2022-04-03 13:27:58 -0400

    working to develop test

---
+++ capstone/src/test/java/com/techelevator/view/PurchaseMenuTest.java
@@ -0,0 +1,35 @@
+package com.techelevator.view;
+
+import static org.junit.Assert.*;
+
+import com.techelevator.data.Logger;
+import com.techelevator.inventory.Item;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+
+public class PurchaseMenuTest {
+//    private Menu menu;
+//    private DisplayMenu displayMenu;
+//    private PurchaseMenu purchaseMenu;
+//    private Mockito mockito;
+//
+//    @Test
+//    public void does_switch_case_m_call_method_and_loop(){
+//        PurchaseMenu trial = Mockito.mock(PurchaseMenu.class);
+//        Logger log = Mockito.mock(Logger.class);
+//        trial.selectMenu("M");
+//        log.moneyFed();
+//
+//
+//        Mockito.verify(trial,Mockito)
+//    }
+}
