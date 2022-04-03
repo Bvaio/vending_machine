4863f5f4832534fbf8efee19326c24acbd7f5f26
Thomas Lee <thmasleemail@gmail.com>
2022-04-02 21:51:46 -0400

    think DisplayMenuTest done

--- capstone/src/test/java/com/techelevator/view/DisplayMenuTest.java
+++ capstone/src/test/java/com/techelevator/view/DisplayMenuTest.java
@@ -1,18 +1,39 @@
 package com.techelevator.view;
 
+import com.techelevator.inventory.Item;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
 public class DisplayMenuTest {
     Menu menu;
     DisplayMenu displayMenu;
+    String filePath = "src/test/java/com/techelevator/testFiles/validTestFile.csv";
+    Map< String, Item > displayItemMap;
+    String[] expectedKeys;
 
     @Before
     public void setUp() throws Exception {
         menu = new Menu();
+        menu.getInventory().createItemMap( filePath );
+
+        displayMenu = new DisplayMenu();
+        displayItemMap = displayMenu.getInventory().getItemMap();
+
+        expectedKeys = new String[] {
+                "A1", "A2", "A3", "A4",
+                "B1", "B2", "B3", "B4",
+                "C1", "C2", "C3", "C4",
+                "D1", "D2", "D3", "D4",
+                "E1", "E2", "E3", "E4",
+                "F1", "F2", "F3", "F4",
+                "G1", "G2", "G3", "G4"
+        };
     }
 
     @After
@@ -20,36 +41,51 @@ public class DisplayMenuTest {
     }
 
     @Test
-    public void has_access_to_inventory_slot_keys() {
-        displayMenu = new DisplayMenu();
-        menu.getInventory().createItemMap( "test.csv" );
-
-        String[] expectedKeys = new String[] {
-                "A1", "A2", "A3", "A4",
-                "B1", "B2", "B3", "B4",
-                "C1", "C2", "C3", "C4",
-                "D1", "D2", "D3", "D4"
-        };
+    public void displayMenu_has_access_to_inventory_slot_keys() {
 
         for ( String key : expectedKeys ) {
-            assertTrue( displayMenu.getInventory().getItemMap().containsKey( key ) );
+            assertTrue( displayItemMap.containsKey( key ) );
         }
+
     }
 
     @Test
-    public void has_access_to_inventory_item_names() {
-        displayMenu = new DisplayMenu();
+    public void displayMenu_has_access_to_inventory_item_names() {
+        String A1Name = "Nachos";
+        String B1Name = "Chippos";
+        String C1Name = "Popcorn";
+        String D1Name = "Preengles";
 
-        String[] expectedKeys = new String[] {
-                "A1", "A2", "A3", "A4",
-                "B1", "B2", "B3", "B4",
-                "C1", "C2", "C3", "C4",
-                "D1", "D2", "D3", "D4"
-        };
+        assertEquals( A1Name, displayItemMap.get( "A1" ).getItemName() );
+        assertEquals( B1Name, displayItemMap.get( "B1" ).getItemName() );
+        assertEquals( C1Name, displayItemMap.get( "C1" ).getItemName() );
+        assertEquals( D1Name, displayItemMap.get( "D1" ).getItemName() );
+    }
 
-        for ( String key : expectedKeys ) {
-            assertTrue( displayMenu.getInventory().getItemMap().containsKey( key ) );
-        }
+    @Test
+    public void displayMenu_has_access_to_inventory_item_types() {
+        String A1Type = "Munchy";
+        String A2Type = "Sandwich";
+        String A3Type = "Drink";
+        String A4Type = "Dessert";
+
+        assertEquals( A1Type, displayItemMap.get( "A1" ).getItemType() );
+        assertEquals( A2Type, displayItemMap.get( "A2" ).getItemType() );
+        assertEquals( A3Type, displayItemMap.get( "A3" ).getItemType() );
+        assertEquals( A4Type, displayItemMap.get( "A4" ).getItemType() );
+    }
+
+    @Test
+    public void displayMenu_has_access_to_inventory_item_prices() {
+        BigDecimal A1Prices = BigDecimal.valueOf( 3.85 );
+        BigDecimal A2Prices = BigDecimal.valueOf( 5.25 );
+        BigDecimal A3Prices = BigDecimal.valueOf( 2.35 );
+        BigDecimal A4Prices = BigDecimal.valueOf( 1.75 );
+
+        assertEquals( A1Prices, displayItemMap.get( "A1" ).getItemPrice() );
+        assertEquals( A2Prices, displayItemMap.get( "A2" ).getItemPrice() );
+        assertEquals( A3Prices, displayItemMap.get( "A3" ).getItemPrice() );
+        assertEquals( A4Prices, displayItemMap.get( "A4" ).getItemPrice() );
     }
 
 }
